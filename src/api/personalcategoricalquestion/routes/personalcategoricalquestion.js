e7dcb728442e66bf6212f1b0b1091b185e0fb7a1
phaks323@gmail.com <phaks323@gmail.com>
2023-11-17 16:13:04 +0200

    fixed question type

---
+++ src/api/personalcategoricalquestion/routes/personalcategoricalquestion.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * personalcategoricalquestion router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::personalcategoricalquestion.personalcategoricalquestion');
